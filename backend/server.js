74bddb6bdc268324257fff8c3974296b4e626cc2
sonia329 <soniashehzadi379@gmail.com>
2026-06-17 09:47:18 +0500

    Render port fix

--- backend/server.js
+++ backend/server.js
@@ -234,7 +234,8 @@ app.get("/", (req, res) => {
   res.send("🚀 Galaxy Explorer OTP Server Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
